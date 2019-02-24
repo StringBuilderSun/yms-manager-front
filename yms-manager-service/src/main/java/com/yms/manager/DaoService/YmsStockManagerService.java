bdad15d159e0a29894343a4eb3c8e6a8fd7f68fc
StringBuilder <lijinpeng-it@bestpay.cpm.cn>
2019-02-24 21:25:25 +0800

    add message

--- yms-manager-service/src/main/java/com/yms/manager/DaoService/YmsStockManagerService.java
+++ yms-manager-service/src/main/java/com/yms/manager/DaoService/YmsStockManagerService.java
@@ -1,51 +1,44 @@
 package com.yms.manager.DaoService;
 
-import com.market.stock.model.StockManagerRequest;
-import com.market.stock.model.StockManagerResponse;
-import com.market.stock.service.StockManagerService;
-import com.yms.manager.model.ServiceResult;
-import com.yms.utils.dubbo.DubboResult;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by lijinpeng on 2018/5/24.
  */
-@Service
 @Slf4j
 public class YmsStockManagerService {
-    @Autowired
-    private StockManagerService stockManagerService;
-
-    /**
-     * 根据keyId 查询实体
-     *
-     * @param request    请求参数
-     * @param traceLogId 日志id
-     * @return
-     */
-    public ServiceResult findByKeyId(StockManagerRequest request, String traceLogId) {
-        log.info("调用数据仓储请求:{}", request);
-        DubboResult<StockManagerResponse> response = null;
-        try {
-            response = stockManagerService.StockDataBaseService(request, traceLogId);
-        } catch (Exception e) {
-            log.error("远程服务调用超时!!!");
-            return new ServiceResult(null, "SYS999990", "服务超时");
-        }
-        log.info("服务响应结果:{}", response);
-        if (response.isSuccess() == true) {
-            StockManagerResponse result = response.getResult();
-            if (request != null) {
-                return new ServiceResult(result.getResponseModel(), result.getResponseCode(), result.getResponseDesc());
-            } else {
-                log.error("远程服务调用，系统内部异常！");
-                return new ServiceResult(null, "SYS999999", "服务内部异常");
-            }
-        } else {
-            log.info("服务调用失败！！！");
-            return null;
-        }
-    }
+//    @Autowired
+//    private StockManagerService stockManagerService;
+//
+//    /**
+//     * 根据keyId 查询实体
+//     *
+//     * @param request    请求参数
+//     * @param traceLogId 日志id
+//     * @return
+//     */
+//    public ServiceResult findByKeyId(StockManagerRequest request, String traceLogId) {
+//        log.info("调用数据仓储请求:{}", request);
+//        DubboResult<StockManagerResponse> response = null;
+//        try {
+//            response = stockManagerService.StockDataBaseService(request, traceLogId);
+//        } catch (Exception e) {
+//            log.error("远程服务调用超时!!!");
+//            return new ServiceResult(null, "SYS999990", "服务超时");
+//        }
+//        log.info("服务响应结果:{}", response);
+//        if (response.isSuccess() == true) {
+//            StockManagerResponse result = response.getResult();
+//            if (request != null) {
+//                return new ServiceResult(result.getResponseModel(), result.getResponseCode(), result.getResponseDesc());
+//            } else {
+//                log.error("远程服务调用，系统内部异常！");
+//                return new ServiceResult(null, "SYS999999", "服务内部异常");
+//            }
+//        } else {
+//            log.info("服务调用失败！！！");
+//            return null;
+//        }
+//    }
 }
--- yms-manager-web/src/main/java/com/yms/market/controller/UserController.java
+++ yms-manager-web/src/main/java/com/yms/market/controller/UserController.java
@@ -1,13 +1,6 @@
 package com.yms.market.controller;
 
 import com.alibaba.dubbo.common.json.JSON;
-import com.market.stock.enums.RequestType;
-import com.market.stock.enums.TablesEnum;
-import com.market.stock.model.DTO.User;
-import com.market.stock.model.StockManagerRequest;
-import com.yms.manager.DaoService.YmsStockManagerService;
-import com.yms.manager.model.ServiceResult;
-import com.yms.utils.log.AlivnMarker;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,33 +25,6 @@ public class UserController {
     @RequestMapping("/login")
     @ResponseBody
     public String login(String uid) {
-        String traceLogId = UUID.randomUUID().toString();
-        MDC.put(AlivnMarker.TRACE_LOG_ID, traceLogId);
-        log.info("用户请求登陆，用户id:{}", uid);
-        StockManagerRequest request = new StockManagerRequest();
-        request.setRequestType(RequestType.SingleQuery);
-        request.setTablesEnum(TablesEnum.USER_TABLE);
-        User user = new User();
-        user.setUid(Integer.parseInt(uid));
-        request.setDataModel(user);
-        User user00=new User();
-        user00.setUname("lijinpeng");
-        user00.setSex(false);
-        user00.setPhone("1893573846");
-        user00.setCity("河南郑州");
-        ServiceResult<User> result = new ServiceResult<User>(user00);//ymsStockManagerService.findByKeyId(request, traceLogId);
-        if (result.getResult() != null) {
-            log.info("用户登陆成功！{}", result.getResult());
-            String jsonString = null;
-            try {
-                jsonString = JSON.json(result.getResult());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return jsonString;
-        } else {
-            log.info("响应码:{},响应描述:{}", result.getRspCode(), result.getRspMsg());
-            return result.toString();
-        }
+        return uid + "-云梦丝-登录成功！";
     }
 }
